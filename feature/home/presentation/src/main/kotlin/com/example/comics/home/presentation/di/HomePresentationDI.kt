4f6d2dde5f9e91d374ca04329a3b1245869c15db
ruiderson <ruiderson@live.com>
2025-09-14 19:17:20 -0300

    feat: adds registration feature

--- feature/home/presentation/src/main/kotlin/com/example/comics/home/presentation/di/HomePresentationDI.kt
+++ feature/home/presentation/src/main/kotlin/com/example/comics/home/presentation/di/HomePresentationDI.kt
@@ -2,19 +2,10 @@ package com.example.comics.home.presentation.di
 
 import com.example.comics.home.presentation.home.interaction.HomeViewModel
 import com.example.comics.home.presentation.registration.interaction.RegistrationViewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val homePresentationModule = module {
-    factory {
-        HomeViewModel(
-            getComicsUseCase = get(),
-            dispatchers = get()
-        )
-    }
-
-    factory {
-        RegistrationViewModel(
-            savedStateHandle = get()
-        )
-    }
+    viewModelOf(::HomeViewModel)
+    viewModelOf(::RegistrationViewModel)
 }
